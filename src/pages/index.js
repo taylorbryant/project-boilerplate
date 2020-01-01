9348292d05c451de06f25b7720ba2aea0606401f
Taylor Bryant <taylor@bryant.io>
2020-01-01 17:31:44 -0600

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import "../css/index.css";
+
+import React from "react";
+
+function Home() {
+  return (
+    <main className="bg-indigo-600 flex items-center justify-center min-h-screen text-white">
+      <h1 className="font-black text-6xl tracking-tight">
+        Product Boilerplate
+      </h1>
+    </main>
+  );
+}
+
+export default Home;
